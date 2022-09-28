bfe09a072a6fabaeb1e6b7cbdc1b98c781a9b069
ferOlguiin <feergui997@hotmail.com>
2022-09-27 22:03:24 -0300

    cambiosEnProyectos

--- pages/views/proyectos.jsx
+++ pages/views/proyectos.jsx
@@ -13,7 +13,7 @@ const Proyectos = () => {
       <h1 className="text-center text-white fw-bold mb-3">Visitá algunos de mis proyectos</h1>
         <div className="container d-flex justify-content-center flex-sm-row flex-column align-items-center">
             <div className="container-fluid d-flex flex-column align-items-center m-1">
-              <a className="text-decoration-none" href="https://app-post-more-image.herokuapp.com/" rel="noreferrer" target="_blank">
+              <a className="text-decoration-none" href="https://ecommerce-example-app.herokuapp.com/" rel="noreferrer" target="_blank">
                 <Image
                   className="imgProyect"
                   src={picPng}
@@ -22,13 +22,13 @@ const Proyectos = () => {
                   height={200}
                 />
                 </a>
-                <h5 className="text-center text-white fw-bold text-break">Proyecto: CRUD con M-E-R-N</h5>
+                <h5 className="text-center text-white fw-bold text-break">Proyecto: E-Commerce de ejemplo</h5>
                 <div className="d-flex justify-content-center align-items-center">
-                  <a className="m-0 p-0 text-decorartion-none" href="https://github.com/ferOlguiin/mern-stack-crud-image" rel="noreferrer" target="_blank"><BsGithub className="text-white fs-2 mx-1"/></a>
+                  <a className="m-0 p-0 text-decorartion-none" href="https://github.com/ferOlguiin/ecommerce-example" rel="noreferrer" target="_blank"><BsGithub className="text-white fs-2 mx-1"/></a>
                 </div>
             </div>
             <div className="container-fluid d-flex flex-column align-items-center m-1">
-              <a className="text-decoration-none" href="https://ferolguiin.github.io/Sorteo_Simple/" rel="noreferrer" target="_blank">
+              <a className="text-decoration-none" href="https://note-app-mernproyect.herokuapp.com/" rel="noreferrer" target="_blank">
                 <Image
                   className="imgProyect"
                   src={picPng}
@@ -37,58 +37,9 @@ const Proyectos = () => {
                   height={200}
                 />
                 </a>
-                <h5 className="text-center text-white fw-bold text-break">Proyecto: Sorteo JS</h5>
+                <h5 className="text-center text-white fw-bold text-break">Proyecto: App de notas</h5>
                 <div className="d-flex justify-content-center align-items-center">
-                  <a className="m-0 p-0 text-decorartion-none" href="https://github.com/ferOlguiin/Sorteo_Simple" rel="noreferrer" target="_blank"><BsGithub className="text-white fs-2 mx-1"/></a>
-                </div>
-            </div>
-            <div className="container-fluid d-flex flex-column align-items-center m-1">
-              <a className="text-decoration-none" href="https://ferolguiin.github.io/Conversor-de-numeros-romanos/" rel="noreferrer" target="_blank">
-                <Image
-                  className="imgProyect"
-                  src={picPng}
-                  alt="image"
-                  width={200}
-                  height={200}
-                />
-              </a>
-              <h5 className="text-center text-white fw-bold text-break">Proyecto: Conversor romano</h5>
-              <div className="d-flex justify-content-center align-items-center">
-                  <a className="m-0 p-0 text-decorartion-none" href="https://github.com/ferOlguiin/Conversor-de-numeros-romanos" rel="noreferrer" target="_blank"><BsGithub className="text-white fs-2 mx-1"/></a>
-                </div>
-            </div>
-            
-            
-        </div>
-        <div className="container d-flex justify-content-center flex-sm-row flex-column align-items-center mt-2">
-            <div className="container-fluid d-flex flex-column align-items-center m-1">
-              <a className="text-decoration-none" href="https://ferolguiin.github.io/React-Api-Pagination/" rel="noreferrer" target="_blank">
-                <Image
-                  className="imgProyect"
-                  src={picPng}
-                  alt="image"
-                  width={200}
-                  height={200}
-                />
-              </a>
-              <h5 className="text-center text-white fw-bold text-break">Proyecto: Consumo de Api-Paginacion con React</h5>
-              <div className="d-flex justify-content-center align-items-center">
-                  <a className="m-0 p-0 text-decorartion-none" href="https://github.com/ferOlguiin/React-Api-Pagination" rel="noreferrer" target="_blank"><BsGithub className="text-white fs-2 mx-1"/></a>
-                </div>
-            </div>
-            <div className="container-fluid d-flex flex-column align-items-center m-1">
-              <a className="text-decoration-none" href="https://node-express-mongo-crud1.herokuapp.com/" rel="noreferrer" target="_blank">
-                <Image
-                  className="imgProyect"
-                  src={picPng}
-                  alt="image"
-                  width={200}
-                  height={200}
-                />
-              </a>
-              <h5 className="text-center text-white fw-bold text-break">Proyecto: CRUD con NodeJs-Express-MongoDB-Handlebars</h5>
-              <div className="d-flex justify-content-center align-items-center">
-                  <a className="m-0 p-0 text-decorartion-none" href="https://github.com/ferOlguiin/nodejs-express-hbs-mongodb-crud" rel="noreferrer" target="_blank"><BsGithub className="text-white fs-2 mx-1"/></a>
+                  <a className="m-0 p-0 text-decorartion-none" href="https://github.com/ferOlguiin/Note-App" rel="noreferrer" target="_blank"><BsGithub className="text-white fs-2 mx-1"/></a>
                 </div>
             </div>
             <div className="container-fluid d-flex flex-column align-items-center m-1">
@@ -104,6 +55,55 @@ const Proyectos = () => {
               <h5 className="text-center text-white fw-bold text-break">Proyecto: App con Nextjs y NextAuth</h5>
               <div className="d-flex justify-content-center align-items-center">
                   <a className="m-0 p-0 text-decorartion-none" href="https://github.com/ferOlguiin/nextjs-app-auth" rel="noreferrer" target="_blank"><BsGithub className="text-white fs-2 mx-1"/></a>
+                </div>
+            </div>
+            
+            
+        </div>
+        <div className="container d-flex justify-content-center flex-sm-row flex-column align-items-center mt-2">
+            <div className="container-fluid d-flex flex-column align-items-center m-1">
+              <a className="text-decoration-none" href="https://app-post-more-image.herokuapp.com/" rel="noreferrer" target="_blank">
+                <Image
+                  className="imgProyect"
+                  src={picPng}
+                  alt="image"
+                  width={200}
+                  height={200}
+                />
+              </a>
+              <h5 className="text-center text-white fw-bold text-break">Proyecto: CRUD con imágenes</h5>
+              <div className="d-flex justify-content-center align-items-center">
+                  <a className="m-0 p-0 text-decorartion-none" href="https://github.com/ferOlguiin/mern-stack-crud-image" rel="noreferrer" target="_blank"><BsGithub className="text-white fs-2 mx-1"/></a>
+                </div>
+            </div>
+            <div className="container-fluid d-flex flex-column align-items-center m-1">
+              <a className="text-decoration-none" href="https://ferolguiin.github.io/Sorteo_Simple/" rel="noreferrer" target="_blank">
+                <Image
+                  className="imgProyect"
+                  src={picPng}
+                  alt="image"
+                  width={200}
+                  height={200}
+                />
+              </a>
+              <h5 className="text-center text-white fw-bold text-break">Proyecto: App de sorteos</h5>
+              <div className="d-flex justify-content-center align-items-center">
+                  <a className="m-0 p-0 text-decorartion-none" href="https://github.com/ferOlguiin/Sorteo_Simple" rel="noreferrer" target="_blank"><BsGithub className="text-white fs-2 mx-1"/></a>
+                </div>
+            </div>
+            <div className="container-fluid d-flex flex-column align-items-center m-1">
+              <a className="text-decoration-none" href="https://ferolguiin.github.io/React-Api-Pagination/" rel="noreferrer" target="_blank">
+                <Image
+                  className="imgProyect"
+                  src={picPng}
+                  alt="image"
+                  width={200}
+                  height={200}
+                />
+              </a>
+              <h5 className="text-center text-white fw-bold text-break">Proyecto: React - Api con paginación</h5>
+              <div className="d-flex justify-content-center align-items-center">
+                  <a className="m-0 p-0 text-decorartion-none" href="https://github.com/ferOlguiin/React-Api-Pagination" rel="noreferrer" target="_blank"><BsGithub className="text-white fs-2 mx-1"/></a>
                 </div>
             </div>
         </div>
